bd17c169d758a026ad39d9f6210518b2dcba86d3
reddypandu <saipandupatt143@gmail.com>
2024-05-17 19:38:49 +0530

    p

--- js/medicina.js
+++ js/medicina.js
@@ -74,6 +74,143 @@ jQuery(document).ready(function($) {
 				}
 			}
 		});
+
+		$(document).ready(function () {
+  
+			$("#contact_form").submit(function (event) {
+		  
+			  event.preventDefault();
+		  
+			  var FullName = $('.cname').val();
+			  var Email = $('.cemail').val();
+			  var Phone = $('.Cph').val();
+			  var Company = $('.Ccompany').val();
+			  var Country = $('.Ccountry').val();
+			  var Message = $('.cMessage').val();
+			  var captcha = grecaptcha.getResponse();
+		  
+			  var email_regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+		  
+			  if (FullName.trim() == '') {
+				$(".cname_error").html("This Field is required");
+			  }
+			  else {
+				$('.cname_error').html('');
+			  }
+		  
+			  if (Email.trim() == "") {
+				$(".cemail_error").html("This Field is required");
+			  }
+			  else if (!(Email.match(email_regex)) == true) {
+				$(".cemail_error").html("Please Enter Valid Email");
+			  }
+			  else {
+				$('.cemail_error').html('');
+			  }
+		  
+			  if (Phone.trim() == "") {
+				$(".Cph_error").html("This Field is required");
+			  }
+			  else {
+				$('.Cph_error').html("");
+			  }
+		  
+			  if (Company.trim() == "") {
+				$(".Ccompany_error").html("This Field is required");
+			  }
+			  else {
+				$('.Ccompany_error').html("");
+			  }
+		  
+			  if (Country.trim() == "") {
+				$(".Ccountry_error").html("This Field is required");
+			  }
+			  else {
+				$('.Ccountry_error').html("");
+			  }
+		  
+			  if (Message.trim() == "") {
+				$(".cMessage_error").html("This Field is required");
+			  }
+			  else {
+				$('.cMessage_error').html("");
+			  }
+		  
+			  if (!(FullName.length == 0 || Email.length == 0 || Phone.length == 0 || Company.length == 0 || Country.length == 0 || Message.length == 0) && (!Email.match(email_regex) == false)) {
+				$.ajax({
+				  type: "POST",
+				  url: "act.php",
+				  data: {
+					cname: FullName,
+					cemail: Email,
+					Cph: Phone,
+					Ccompany: Company,
+					Ccountry: Country,
+					cMessage: Message,
+					recaptchaResponse: captcha
+				  },
+				  success: function (response) {
+					if (response.success == true) {
+					  window.location.href = "contact.html"
+					} else {
+					  console.log("Processing failed: ");
+					}
+		  
+				  }
+				});
+			  }
+			});
+		  
+		  
+			// $('.cname').keydown(function (e) {
+			//   if (e.altKey) {
+			// 	e.preventDefault();
+			//   } else {
+			// 	var key = e.keyCode;
+			// 	if (!((key == 8) || (key == 32) || (key == 46) || (key == 16) || (key >= 35 && key <= 40) || (key >= 65 && key <= 90))) {
+			// 	  e.preventDefault();
+			// 	}
+			//   }
+			// });
+		  
+		
+		  
+			// $('.Ccountry,.Ccompany').on("input", function () {
+			//   var inputValue = $(this).val();
+			//   var validCharacters = /^[A-Za-z]+$/;
+		  
+			//   if (!validCharacters.test(inputValue)) {
+			// 	$(this).val(inputValue.replace(/[^A-Za-z]/g, ""));
+			//   }
+			// });
+		  
+			// $('.Cph').on("input", function () {
+			//   var inputValue = $(this).val();
+			//   var validNumbers = /^\d+$/;
+		  
+			//   if (!validNumbers.test(inputValue) || inputValue.length !== 15) {
+			// 	$(this).val(inputValue.replace(/\D/g, "").substring(0, 15));
+			//   }
+			// });
+		  
+			// const textArea = document.getElementById("textArea");
+			// let previousChar = '';
+		  
+			// textArea.addEventListener("input", function (e) {
+			//   const inputValue = textArea.value;
+			//   const currentChar = inputValue.charAt(inputValue.length - 1);
+		  
+			//   if (currentChar === ' ' && previousChar === ' ') {
+			// 	// If the current and previous characters are spaces, prevent the input
+			// 	e.preventDefault();
+			// 	textArea.value = inputValue.slice(0, inputValue.length - 1); // Remove the extra space
+			//   }
+		  
+			//   previousChar = currentChar;
+			// });
+		  
+		  
+		  });
 		
 });
 	
